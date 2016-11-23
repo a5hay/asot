8807332d5bd80f1e4132e8e6e4d878da144dd6ec
a5hay <ashaymanjure@gmail.com>
2016-11-22 23:18:43 -0800

    modified readme and editied script for better messaging

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 
 This script lets you download the latest one file episode of a State of trance from http://www.asotarchive.org/.
 
+This script was originally inspired from [https://github.com/HuwSy/ASOT-Archive], but I had to modify it to make it work for the new redesign on asotarchive.
 
+###Requirements
+
+`curl`
+
+`wget`
+
+*nix based machine. I have not tested it on Cygwin, but my guess it will work as long as you have `curl` and `wget`
+
+### How to run.
+
+`git clone https://github.com/a5hay/asot.git`
+
+`chmod +x a-state-of-trance.sh`
+
+`./a-state-of-trance.sh`
+
+
+if you have any feedback, please message me on github.
--- a-state-of-trance.sh
+++ a-state-of-trance.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #/bin/bash
 
 # script to download latest episode for the radio show A State of Trance by Armin van buuren
@@ -10,7 +11,7 @@ fi
 
 EP=`curl -v --silent http://www.asotarchive.org --stderr - | grep '[/]episode[/][\?]p=[0-9]*' -o | head -n 2 | tail -n 1`
 NEWEP=`curl -Ls -o /dev/null -w %{url_effective} http://www.asotarchive.org$EP`
-echo $NEWEP
+
 CU=`echo $NEWEP | grep -Eo '[0-9]{1,4}'`
 LA=`cat ASOT.log`
 echo "Current episode"
@@ -18,9 +19,8 @@ echo $CU
 echo "Last download"
 echo $LA
 if [ "$LA" \< "$CU" ]; then
-  echo "New download"
+  echo "Downloading latest episode"
   RD=`curl -v --silent $NEWEP --stderr - |  grep download\/asotarchive | sed -n 's/.*href="\([^"]*\).*/\1/p'`
-#  echo $RD
   wget -c $RD
   echo $CU > ASOT.log
 fi
