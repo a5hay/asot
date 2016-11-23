71a9296de8d0bb94c88c39fedbb4875bc227b850
a5hay <ashaymanjure@gmail.com>
2016-11-22 23:26:11 -0800

    modified readme and editied script for better messaging + shebang

--- a-state-of-trance.sh
+++ a-state-of-trance.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#/bin/bash
 
 # script to download latest episode for the radio show A State of Trance by Armin van buuren
 # inspired by https://github.com/HuwSy/ASOT-Archive and fixed the issues it had
@@ -11,7 +10,7 @@ fi
 
 EP=`curl -v --silent http://www.asotarchive.org --stderr - | grep '[/]episode[/][\?]p=[0-9]*' -o | head -n 2 | tail -n 1`
 NEWEP=`curl -Ls -o /dev/null -w %{url_effective} http://www.asotarchive.org$EP`
-
+echo $NEWEP
 CU=`echo $NEWEP | grep -Eo '[0-9]{1,4}'`
 LA=`cat ASOT.log`
 echo "Current episode"
@@ -19,8 +18,9 @@ echo $CU
 echo "Last download"
 echo $LA
 if [ "$LA" \< "$CU" ]; then
-  echo "Downloading latest episode"
+  echo "New download"
   RD=`curl -v --silent $NEWEP --stderr - |  grep download\/asotarchive | sed -n 's/.*href="\([^"]*\).*/\1/p'`
+#  echo $RD
   wget -c $RD
   echo $CU > ASOT.log
 fi
